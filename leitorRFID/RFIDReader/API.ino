a790ac4509d0f8cbe2369e9569d0429527b56bc8
Felipe Salgueiro Oliveira e Silva <felipe.salgueiro.o.s@gmail.com>
2013-03-19 23:38:23 -0300

    API do RFID , modularizada.
    Ainda faltam muitas instruções.

--- leitorRFID/RFIDReader/API.ino
+++ leitorRFID/RFIDReader/API.ino
@@ -1,13 +1,8 @@
-
-#define PRESET_VALUE 0xFFFF
-#define POLYNOMIAL  0x8408
-
-
-int i = 0;
-
 const unsigned char getReaderInfo[] = {0x04, 0x00, 0x21};
 const unsigned char inventory[] = {0x04, 0x00, 0x01};
 const unsigned char buzzer[] = {0x07, 0x00, 0x33, 0, 0, 0};
+
+
 
 unsigned int uiCrc16Cal(unsigned char const  * pucY, unsigned char ucX)
 {
@@ -36,12 +31,29 @@ void enviarChecksum(unsigned char const  * buffer, unsigned char len, HardwareSe
   unsigned int crc = uiCrc16Cal(buffer, len);
   unsigned char CRC_MSB = 0;
   unsigned char CRC_LSB = 0;
- 
+
   CRC_MSB = (crc & 0x0000FF00) >> 8;
   CRC_LSB = crc & 0x000000FF;
-  
+
   (*porta).write(CRC_LSB);
   (*porta).write(CRC_MSB);
+}
+
+int checarChecksum(unsigned char const  *buffer, unsigned short len, unsigned char CRC_LSB, unsigned char CRC_MSB){
+  unsigned int crc = uiCrc16Cal(buffer, len);
+  unsigned char NEW_CRC_MSB = 0;
+  unsigned char NEW_CRC_LSB = 0;
+  int retorno = 0;
+
+  NEW_CRC_MSB = (crc & 0x0000FF00) >> 8;
+  NEW_CRC_LSB = crc & 0x000000FF;
+  
+  if(CRC_MSB == NEW_CRC_MSB && CRC_LSB == NEW_CRC_LSB)
+  {
+	  retorno = 1;
+  }
+
+  return retorno;
 }
 
 void print_serial()
@@ -56,49 +68,4 @@ void print_serial()
     }
   }
 }
-
-void setup()  
-{
-  
-  // Open serial communications and wait for port to open:
-  Serial.begin(57600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
-
-
-  Serial.println("InicioSerial0 ");
-
-  Serial1.begin(57600);
-  while (!Serial1) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
-
-
-  Serial.println("Pedindo informacoes do leitor...");
-    Serial1.write(getReaderInfo, 3);
-    enviarChecksum(getReaderInfo, 3, &Serial1);
-    print_serial();
-  
-  /* Serial.println("Desligando o buzzer do leitor..."); //Não funciona =/, só serve pra se quiser apitar o buzzer interno (alterando os parametros).
-   Serial1.write(buzzer, 6);
-    enviarChecksum(buzzer, 6, &Serial1);
-    print_serial();  */
-  
-}
-
-void loop(){
-    Serial.println("Pedindo inventorio do leitor...");
-    Serial1.write(inventory, 3);
-    enviarChecksum(inventory, 3, &Serial1);
-
-    print_serial();
-  delay(3000); 
-  
-
-  delay (200);  
-  
- 
-}
-
 
