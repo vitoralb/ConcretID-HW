bcd3dd9912381c0acdbbfc4761e80d161468202c
vitorqa <vitor.q.albuquerque@gmail.com>
2013-04-22 07:29:19 -0300

    Teste Broadcast

---
+++ cc2530/contiki_os/examples/cc2530dk/projeto-edna/fixo.c
@@ -0,0 +1,141 @@
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+#include "contiki.h"
+#include "contiki-lib.h"
+#include "contiki-net.h"
+
+#include <string.h>
+#include "dev/leds.h"
+#include "dev/button-sensor.h"
+#include "debug.h"
+
+#define DEBUG DEBUG_PRINT
+#include "net/uip-debug.h"
+
+#define SEND_INTERVAL		2 * CLOCK_SECOND
+#define MAX_PAYLOAD_LEN		40
+
+static char buf[MAX_PAYLOAD_LEN];
+
+/* Our destinations and udp conns. One link-local and one global */
+#define LOCAL_CONN_PORT 3001
+static struct uip_udp_conn *l_conn;
+
+static struct uip_udp_conn *broadcast_conn;
+
+
+/*---------------------------------------------------------------------------*/
+PROCESS(udp_client_process, "UDP client process");
+#if BUTTON_SENSOR_ON
+PROCESS_NAME(ping6_process);
+AUTOSTART_PROCESSES(&udp_client_process, &ping6_process);
+#else
+AUTOSTART_PROCESSES(&udp_client_process);
+#endif
+/*---------------------------------------------------------------------------*/
+static void
+tcpip_handler(void)
+{
+  leds_on(LEDS_GREEN);
+  if(uip_newdata()) {
+    putstring("0x");
+    puthex(uip_datalen());
+    putstring(" bytes response=0x");
+    puthex((*(uint16_t *) uip_appdata) >> 8);
+    puthex((*(uint16_t *) uip_appdata) & 0xFF);
+    putchar('\n');
+  }
+  leds_off(LEDS_GREEN);
+  return;
+}
+/*---------------------------------------------------------------------------*/
+static void
+timeout_handler(void)
+{
+  static int seq_id = 0;
+  struct uip_udp_conn *this_conn;
+
+  leds_on(LEDS_RED);
+  memset(buf, 0, MAX_PAYLOAD_LEN);
+
+  PRINTF("Client to: ");
+  PRINT6ADDR(&broadcast_conn->ripaddr);
+
+  memcpy(buf, &seq_id, sizeof(seq_id));
+
+  PRINTF(" Remote Port %u,", UIP_HTONS(broadcast_conn->rport));
+  PRINTF(" (msg=0x%04x), %u bytes\n", *(uint16_t *) buf, sizeof(seq_id));
+
+  uip_udp_packet_send(broadcast_conn, buf, sizeof(seq_id));
+  seq_id++;
+  leds_off(LEDS_RED);
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(udp_client_process, ev, data)
+{
+  static struct etimer et;
+  uip_ipaddr_t ipaddr;
+
+  PROCESS_BEGIN();
+  PRINTF("UDP client process started\n");
+
+//  uip_ip6addr(&ipaddr, 0xfe80, 0, 0, 0, 0x0212, 0x4b00, 0x01f1, 0xb579);
+
+  uip_create_linklocal_allnodes_mcast(&ipaddr);
+  /* new connection with remote host */
+  broadcast_conn = udp_new(&ipaddr, UIP_HTONS(3000), NULL);
+  if(!broadcast_conn) {
+    PRINTF("udp_new l_conn error.\n");
+  }
+  udp_bind(broadcast_conn, UIP_HTONS(LOCAL_CONN_PORT));
+
+  PRINTF("Link-Local connection with ");
+  PRINT6ADDR(&l_conn->ripaddr);
+  PRINTF(" local/remote port %u/%u\n",
+         UIP_HTONS(l_conn->lport), UIP_HTONS(l_conn->rport));
+
+  l_conn = udp_new(NULL, UIP_HTONS(0), NULL);
+  udp_bind(l_conn, UIP_HTONS(3000));
+
+  etimer_set(&et, SEND_INTERVAL);
+
+  while(1) {
+    PROCESS_YIELD();
+    if(etimer_expired(&et)) {
+      timeout_handler();
+      etimer_restart(&et);
+    } else if(ev == tcpip_event) {
+      tcpip_handler();
+    }
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
