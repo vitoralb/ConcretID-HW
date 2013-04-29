c78e8ed874537db77d62376e7b93136b87ba66cc
marcelo <mcdlima@gmail.com>
2013-04-29 11:56:57 -0300

    chuva do carai

--- Raspberry/marcelo.py
+++ Raspberry/marcelo.py
@@ -9,25 +9,26 @@ class PostSerialData():
 		self.ser = serial.Serial("/dev/" + usb_port , baudrate=self.baud_rate, timeout=1)
 
 	def post_location(self, data): 
-		rfid, lati, longi = data.split(":")[1][:-1].split(",") # pega a string no formato "L:id,lat,longi;" e extrai os 3 dados dela
-		print "postando nova localizacao: " + rfid + ", lati: " + lati + ", longi: " + longi
+		rfid, lati, longi = data.split(":")[1][:data.split(":")[1].index(";")].split(",") # pega a string no formato "L:id,lat,longi;" e extrai os 3 dados dela
+		print "Postando nova localizacao: id:" + rfid + ", lati:" + lati + ", longi:" + longi
 		payload = {"rfid": rfid, "x": longi, "y": lati, "status": ""}
 		req = requests.post(self.url, data=payload)
 		if req.status_code == 200:
-			print "localizacao postada com sucesso"
+			print "Localizacao postada com sucesso\n"
 		else:
-			print "problema no post"
+			print "Problema no post"
 
 	def start_posting(self):
+		print "Aguardando dados..\n"
 		while True:
 			data = self.ser.readline()
-			print "recebeu: " + data
 			if len(data) == 0:
-				print('')
-			elif data[0:2] == 'L:': #and data[-1] ==";": #and data.count(",") == 2 and
+				pass
+			elif data[0:2] == 'L:' and data.count(",") == 2 and (";" in data.split(",")[2]):
+				print "Recebeu " + data
 				self.post_location(data)		
 			else:
-				print "comando invalido"
+				print "Comando invalido -> " + data
 
 if __name__ == "__main__":
 	teste = PostSerialData("ttyUSB0", 9600, "http://rcsr.p.ht/concretid/modulos/peca.update.hw.php")
